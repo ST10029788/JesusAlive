b322cd56a503eb40c07d92a20e439ac58ee75644
ST10030992 <ST10030992@vcconnect.edu.za>
2024-10-24 17:50:41 +0200

    Paper Pdf View Activity UI Test

--- app/src/androidTest/java/com/scriptsquad/reflectionsoffaith/PaperPdfViewActivityUITest.kt
+++ app/src/androidTest/java/com/scriptsquad/reflectionsoffaith/PaperPdfViewActivityUITest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.scriptsquad.reflectionsoffaith
 
 import android.content.Intent
@@ -5,6 +7,11 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Root
+import android.os.IBinder
+import android.view.WindowManager
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.Matcher
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -12,7 +19,6 @@ import com.scriptsquad.reflectionsoffaith.Bibles.activities.PaperPdfViewActivity
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matcher
 
 @RunWith(AndroidJUnit4::class)
 class PaperPdfViewActivityTest {
@@ -69,7 +75,22 @@ class PaperPdfViewActivityTest {
         onView(withText("Failed to get url due to")).inRoot(ToastMatcher()).check(matches(isDisplayed()))
     }
 
-    private fun ToastMatcher(): Matcher<Root> {
-        TODO("Not yet implemented")
+    // ToastMatcher implementation to match Toast messages in UI tests
+    fun ToastMatcher(): Matcher<Root> {
+        return object : TypeSafeMatcher<Root>() {
+            override fun describeTo(description: Description) {
+                description.appendText("is toast")
+            }
+
+            override fun matchesSafely(root: Root?): Boolean {
+                val type = root?.windowLayoutParams?.get()?.type
+                if (type == WindowManager.LayoutParams.TYPE_TOAST) {
+                    val windowToken: IBinder = root.decorView.windowToken
+                    val appToken: IBinder = root.decorView.applicationWindowToken
+                    return windowToken === appToken
+                }
+                return false
+            }
+        }
     }
 }
